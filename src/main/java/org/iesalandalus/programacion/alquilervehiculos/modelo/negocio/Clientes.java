ea34f582c7ccfa30679d5d61bcb0b9fb943a3489
Cuenz <68349561+sergixul77@users.noreply.github.com>
2023-03-14 21:05:21 +0100

    He arreglado los siguientes puntos en Clientes:     •  El atributo coleccionClientes debía ser privado.     • En el método get debías devolver un nuevo ArrayList con el mismo contenido y no la referencia.     • En el método getCantidad simplemente debías utilizar el método size.     •  En el método modificar debías buscar el cliente y sobre el que encuentres realizas las modificaciones.

--- src/main/java/org/iesalandalus/programacion/alquilervehiculos/modelo/negocio/Clientes.java
+++ src/main/java/org/iesalandalus/programacion/alquilervehiculos/modelo/negocio/Clientes.java
@@ -1,6 +1,10 @@
 package org.iesalandalus.programacion.alquilervehiculos.modelo.negocio;
 
 import java.util.ArrayList;
+
+
+// version 0
+
 import java.util.List;
 
 import javax.naming.OperationNotSupportedException;
@@ -9,7 +13,7 @@ import org.iesalandalus.programacion.alquilervehiculos.modelo.dominio.Cliente;
 
 public class Clientes {
 
-	public List<Cliente> coleccionClientes;
+	private List<Cliente> coleccionClientes;
 
 	public Clientes() {
 		coleccionClientes = new ArrayList<>(); // creando la lista
@@ -17,19 +21,12 @@ public class Clientes {
 
 	public List<Cliente> get() {
 
-		return coleccionClientes;
+		return new ArrayList<>(coleccionClientes);
 	}
 
 	public int getCantidad() { // tengo que recorrer la lista y incrementar en cada paso
-
-		int cantidadElementos = 0;
-		for (Cliente cliente : coleccionClientes) {
-
-			cantidadElementos++;
-		}
-
-		return cantidadElementos; // devuelvo el numero de elementos que contiene la lista
-
+		
+		return coleccionClientes.size(); // devuelvo el tamaño de la coleccion
 	}
 
 	public void insertar(Cliente cliente) throws OperationNotSupportedException {
@@ -79,32 +76,34 @@ public class Clientes {
 
 	}
 
-	/*
-	 * Crea el método modificar que permitirá cambiar el nombre o el teléfono (si
-	 * estos parámetros no son nulos ni blancos) de un cliente existente y si no
-	 * lanzará la correspondiente excepción.
-	 */
+	
 
 	public void modificar(Cliente cliente, String nombre, String telefono) throws OperationNotSupportedException {
-
+		
 		if (cliente == null) {
 			throw new NullPointerException("ERROR: No se puede modificar un cliente nulo.");
+		}
+		
+		
+		Cliente clienteEncontrado = buscar(cliente); // esto busca al cliente.
+		
+		
+		if (clienteEncontrado == null) { // si el cliente que buscamos no esta en la lista salta la excepcion 
+			throw new OperationNotSupportedException("ERROR: No existe ningún cliente con ese DNI.");
 		}
 
 		if (telefono != null && !telefono.isBlank()) { // si el telefono no es nulo ni tampoco tiene espacios en blanco
 
-			cliente.setTelefono(telefono);
+			clienteEncontrado.setTelefono(telefono);
 
 		}
 		if (nombre != null && !nombre.isBlank()) { // si el nombre es diferente a nulo y no tiene espacios ya que lo
 													// quito con el isBlank
 
-			cliente.setNombre(nombre);
+			clienteEncontrado.setNombre(nombre); // del cliente encontrado modifico el nombre
 		}
 
-		if (!coleccionClientes.contains(cliente)) {
-			throw new OperationNotSupportedException("ERROR: No existe ningún cliente con ese DNI.");
-		}
+		
 
 	}
 
