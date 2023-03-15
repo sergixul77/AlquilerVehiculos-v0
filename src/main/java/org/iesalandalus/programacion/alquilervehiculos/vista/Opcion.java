0f924ae3a0eae96c53cc5d17ba82dc231aad0ad2
Cuenz <68349561+sergixul77@users.noreply.github.com>
2023-03-15 22:03:20 +0100

    modificado para que este bien en vista las constantes

--- src/main/java/org/iesalandalus/programacion/alquilervehiculos/vista/Opcion.java
+++ src/main/java/org/iesalandalus/programacion/alquilervehiculos/vista/Opcion.java
@@ -7,8 +7,8 @@ public enum Opcion {
 	SALIR("Salir"), INSERTAR_CLIENTE("insertar cliente"), INSERTAR_TURISMO("Insertar turismos"),
 	INSERTAR_ALQUILER("Insertar alquiler"), BUSCAR_CLIENTE("Buscar cliente"), BUSCAR_TURISMO("Buscar turismo"),
 	BUSCAR_ALQUILER("Buscar alquiler"), MODIFICAR_CLIENTE("Modificar cliente"), DEVOLVER_ALQUILER("Devolver alquiler"),
-	BORARR_CLIENTE("Borrar cliente"), BORRAR_TURISMO("Borrar turismo"), BORRAR_ALQUILER("Borrar alquiler"),
-	LISTAR_CLIENTES("Listar clientes"), LISTAR_TURISMO("Listar turismos"), LISTAR_ALQUILERES("Listar alquileres"),
+	BORAR_CLIENTE("Borrar cliente"), BORRAR_TURISMO("Borrar turismo"), BORRAR_ALQUILER("Borrar alquiler"),
+	LISTAR_CLIENTES("Listar clientes"), LISTAR_TURISMOS("Listar turismos"), LISTAR_ALQUILERES("Listar alquileres"),
 	LISTAR_ALQUILERES_CLIENTE("Listar alquiler de un cliente"),
 	LISTAR_ALQUILERES_TURISMO("Lista alquiler de un turismo");
 
